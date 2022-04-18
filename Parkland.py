44df3f1d84b35dc7828d9b779d2415889f481a4e
medsoftware <Ranieri.ripoli@gmail.com>
2022-04-18 22:21:42 +0200

    Update Parkland.py

--- Parkland.py
+++ Parkland.py
@@ -3,4 +3,9 @@ def Parkland():
   peso = float(input("Inserire il peso"))
   sup_ustionata = float(input("inserire la superficie ustionata"))
   return 4*peso*sup_ustionata
+def LiquidToFlow(liquid):
+  first8 = liquid / 16
+  second16 = liquid / 32
+  return first8,second16
+
 
