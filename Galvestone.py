561783c6d1e45fd7a0ec655eb3e857e3dfa5b395
medsoftware <Ranieri.ripoli@gmail.com>
2022-04-18 21:24:46 +0200

    Create Galvestone.py

---
+++ Galvestone.py
@@ -0,0 +1,11 @@
+# Parameter needed are Weight, Height, Burn Area, Sex and Age
+# We will calculate BSA, BMI , BEE - Normal
+# We will calculate Resucitation Fluids and Nutritional requirment
+# Maybe we will check Labs and find some nice algoritms 
+# The sequence could be First Res, Secundary Managment
+
+
+#Glavestone Formula is for children. It uses the Body Surface Area Instead of The Weight
+def Galvestone():
+  # 5000 * BSA + 3000 * BSA_BURN
+  # 
